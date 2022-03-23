9d979ed091e8088f08d0ab7171a4f6ad793c1ae5
whomadewhoandwhy@gmail.com <hard2carry@hotmail.com>
2022-03-23 17:44:34 +0100

    Pp

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Profile from "./profile/ProfileCom";
 
 function App() {
+  let fullName = "Cherif Ahmed";
+  let bio = "work under pressure";
+  let profession = "web dev";
+
+  const handelName = (x) => {
+    alert(x);
+  };
+
   return (
     <>
-    
-    
-    
-    
-    
-    
-    
+      <Profile fullName={fullName} handelName={handelName} bio={bio}>
+        <img
+          className="Profp"
+          style={{ borderRadius: "50%", width: "400px" }}
+          src="https://i.pinimg.com/736x/8b/16/7a/8b167af653c2399dd93b952a48740620.jpg"
+          alt="profile"
+        />
+      </Profile>
     </>
   );
 }
--- src/profile/ProfileCom.js
+++ src/profile/ProfileCom.js
@@ -1,13 +1,14 @@
-const profile component = ({ullName, bio, profession}) => {
-    return (
-        <>
-        
-        
-        
-        
-        
-        
-        
-        </>
-    )
-}
+const Profile = ({ fullName, children, handelName, bio }) => {
+  // const Profile = (props) => {
+  return (
+    <>
+      <div style={{ color: "gray" }}>
+        <h2 onClick={() => handelName(fullName)}>{fullName}</h2>
+        <div>{children}</div>
+        <h3>{bio}</h3>
+      </div>
+    </>
+  );
+};
+
+export default Profile;
